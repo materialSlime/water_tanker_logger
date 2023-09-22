94737c4225b684608e3c4a703757ad7c0b35042d
turing_tested <materialSlime@users.noreply.github.com>
2023-09-22 16:05:18 +0530

    New Branch Init Commit

--- test.py
+++ test.py
@@ -1,21 +1,5 @@
-from datetime import datetime, timedelta
+import csv
+
 import pandas as pd
 
 
-def update_dataframe(name, amt):
-
-    df = pd.read_csv('./logs.csv')
-    filtered = df[(df['Name'] == name) & (df["Payment Status"] == "Pending")]
-
-    print(df[(df['Name'] == name) & (df["Payment Status"] == "Pending")])
-    for index, row in filtered.iterrows():
-
-        if amt >= row['Amount (Rs.)']:
-            df.at[index, 'Payment Status'] = 'Paid'
-            amt -= row['Amount (Rs.)']
-
-    print(df[(df['Name'] == name) & (df["Payment Status"] == "Pending")])
-    df.to_csv('./logs2.csv')
-
-
-update_dataframe("Imrat", 60*5)
